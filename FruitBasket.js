d8a6fac048ef86b67ce7edd2434b305bb7b7c4e5
Link <95591923+LinkGXMasterPlayer2@users.noreply.github.com>
2023-01-08 15:59:16 -0800

    Add files via upload

---
+++ FruitBasket.js
@@ -0,0 +1,38 @@
+status = "";
+let img;
+let detector;
+
+
+function preload(){
+
+    img = loadImage('FruitBasketImage.jpg');
+     detector = ml5.objectDetector('cocossd');
+}
+
+function gotDetections(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);
+    for (let i = 0; i < results.length; i++){
+        let object = results[i];
+        stroke(0,255,0);
+        strokeWeight(4);
+        noFill();
+        rect(object.x, object.y, object.width, object.height);
+    }
+}
+
+function setup(){
+
+    canvas = createCanvas(300,200);
+    canvas.center();
+    image(img, 0, 0);
+    detector.detect(img, gotDetections);
+}
+function modelLoaded(){
+
+    console.log("model Loaded");
+    status = true;
+    objectDetector.detect(gotResult);
+}
